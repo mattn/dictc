e25de353db5e078306f4ab65e63c86f8367f7c4a
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2015-11-17 17:05:58 +0900

    first import

---
+++ README.md
@@ -0,0 +1,23 @@
+# dictc
+
+client for dict protocol server
+
+## Usage
+
+```
+$ dictc -h
+```
+
+## Installation
+
+```
+$ go get github.com/mattn/dictc
+```
+
+## License
+
+MIT
+
+## Author
+
+Yasuhiro Matsumoto (a.k.a mattn)
---
+++ dict.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"golang.org/x/net/dict"
+	"log"
+	"os"
+)
+
+var (
+	ls   = flag.Bool("l", false, "list dictionaries")
+	dn   = flag.String("d", "*", "specify dictionary")
+	addr = flag.String("s", "dict.org:2628", "specify dict server")
+)
+
+func main() {
+	flag.Parse()
+	client, err := dict.Dial("tcp", *addr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if *ls {
+		dicts, err := client.Dicts()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "%v: %v", os.Args[0], err)
+			os.Exit(1)
+		}
+		for _, dict := range dicts {
+			fmt.Println(dict.Name + ":" + dict.Desc)
+		}
+		os.Exit(0)
+	}
+	for _, arg := range flag.Args() {
+		defs, err := client.Define(*dn, arg)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "%v: %v", os.Args[0], err)
+			os.Exit(1)
+		}
+		for _, def := range defs {
+			fmt.Println(string(def.Text))
+		}
+	}
+}
